524947f89c872d9e58a4810d87e48f0ade52db22
chenyuting <357815153@qq.com>
2021-11-02 11:35:02 +0800

    设置路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link, Redirect
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">标签页</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">记账页</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">统计页</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from="/" to="/money"/>
+                    <Route path="*">
+                        <NoMatch/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
+
+ function Statistics(){
+         return <h2> 统计页面</h2>
+     }
+ function Tags(){
+         return <h2> 标签页面</h2>
+     }
+ function Money(){
+         return <h2> 记账页面</h2>
+     }
+ function NoMatch(){
+         return <h2> 404页面</h2>
+     }
 
 export default App;
