b0cb3a13590714531f8b46c525f2f9d1d46ce6e8
chenyuting <357815153@qq.com>
2021-11-02 18:55:12 +0800

    封装Nav 添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,44 @@ import {
     Route,
     Link, Redirect
 } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div`
+
+height: 100vh;
+display: flex;
+flex-direction: column`
+const Main=styled.div`
+   
+  flex-grow: 1;
+  overflow: auto;
+`
+
 
  function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/tags">标签页</Link>
-                        </li>
-                        <li>
-                            <Link to="/money">记账页</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">统计页</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/tags">
-                        <Tags/>
-                    </Route>
-                    <Route path="/money">
-                        <Money/>
-                    </Route>
-                    <Route path="/statistics">
-                        <Statistics/>
-                    </Route>
-                    <Redirect exact from="/" to="/money"/>
-                    <Route path="*">
-                        <NoMatch/>
-                    </Route>
-                </Switch>
-            </div>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from="/" to="/money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav/>
+            </Wrapper>
         </Router>
     );
 }
