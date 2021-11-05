3caa5c770717d34fc33751a7c0a3a203f35ce680
chenyuting <357815153@qq.com>
2021-11-05 16:37:02 +0800

    修复Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,30 +1,51 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
-console.log('12221');
+import Icon from "./Icon";
+
+
+
 const NavWrapper =styled.nav`
   line-height: 24px; 
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul{
   display: flex;
   > li{
+    > a{
+      padding: 4px 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        width: 24px;
+        height: 24px;
+      } 
+    }
     width: 33.33%;
     text-align: center;
-    padding: 16px;
+    
   }
 }`
 const Nav = ()=>{
-    console.log('111');
     return <NavWrapper>
     <ul>
         <li>
-            <Link to="/tags">标签页</Link>
+            <Link to="/tags">
+                <Icon name='tag'/>
+                标签页</Link>
         </li>
         <li>
-            <Link to="/money">记账页</Link>
+
+            <Link to="/money">
+                <Icon name='money'/>
+                记账页</Link>
         </li>
         <li>
-            <Link to="/statistics">统计页</Link>
+
+            <Link to="/statistics">
+                <Icon name='chart'/>
+                统计页</Link>
         </li>
     </ul>
     </NavWrapper>
