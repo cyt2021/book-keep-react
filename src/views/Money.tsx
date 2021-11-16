c551534dfacd31e8ea7ee487558ed79b1ce7db33
chenyuting <357815153@qq.com>
2021-11-16 15:48:43 +0800

    Money.tsx的NotesSection 部分 样式完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,49 +1,77 @@
 import Layout from "../components/Layout";
 import React from "react";
 import styled from "styled-components";
+
 const TagsSection = styled.section`
-    background: #ffffff;padding: 12px 16px;
-> ol {margin: 0 -12px;
->li{
-  background: #D9D9D9;border-radius:18px;
-  display: inline-block;padding: 3px 18px;
-  font-size:14px;margin:8px 12px;
-}
-}
-  > button{
+  background: #ffffff;
+  padding: 12px 16px;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+
+  > button {
     background: none;
-border: none;
-  padding: 2px 4px;
-  border-bottom: 1px solid #333;color:#666;
-  margin-top: 8px;}
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
 
 `
 const NotesSection = styled.section`
-
+background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span{
+      margin-right: 16px;
+      white-space:nowrap;
+    }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 
-const CategorySection  = styled.section`
-    
+const CategorySection = styled.section`
+
 `
 const NumberPadSection = styled.section`
-    
+
 `
 
 function Money() {
     return (<Layout>
-           <TagsSection>
-               <ol>
-                   <li>衣</li>
-                   <li>食</li>
-                   <li>住</li>
-                   <li>行</li>
-               </ol>
-               <button>新增标签</button>
-           </TagsSection>
+            <TagsSection>
+                <ol>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ol>
+                <button>新增标签</button>
+            </TagsSection>
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type='text'/>
+                    <input type='text' placeholder='在这里添加备注'/>
                 </label>
             </NotesSection>
             <CategorySection>
@@ -53,9 +81,9 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberPadSection>
-               <div>
-                   100
-               </div>
+                <div>
+                    100
+                </div>
                 <div>
                     <button>1</button>
                     <button>2</button>
@@ -76,4 +104,5 @@ function Money() {
         </Layout>
     )
 }
+
 export default Money
