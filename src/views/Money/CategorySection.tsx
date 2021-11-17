7d442da9ad11ff98ca9b5121d0fa9d5203dcd6eb
chenyuting <357815153@qq.com>
2021-11-17 18:31:27 +0800

    完成CategorySection的切换功能

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
 font-size: 24px;
   > ul{
     display: flex;
@@ -23,4 +24,20 @@ font-size: 24px;
     }
   }
 `
+const CategorySection :React.FC=() =>{
+    const [category,setCategory]=useState('-')
+    const [categoryList]=useState<('+'|'-')[]>(['+','-'])
+    const categoryMap ={'+':'收入','-':'支出'}
+    return (<Wrapper>
+            <ul>
+                {categoryList.map(c=>
+                    <li className={category===c ?'selected':''}
+                    onClick={()=>{setCategory(c)}}
+                    >{categoryMap[c]}
+                    </li>
+                )}
+            </ul>
+    </Wrapper>
+    )
+}
 export {CategorySection}
