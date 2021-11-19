3307101ce6e6c7d613d6e90e63ace24792fe2a53
chenyuting <357815153@qq.com>
2021-11-19 18:06:22 +0800

    创建/tags/:tag 路由，加入exact 精确匹配

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,13 +1,63 @@
 import Layout from "../components/Layout";
 import React from "react";
 import useTags from "../useTags";
+import styled from "styled-components";
+import Icon from "../components/Icon";
+import {Link} from "react-router-dom";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    margin-left: 16px;
+    > a{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 16px 12px 0;
+    }
+  }
+
+`
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+`
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column
+`
+const Space = styled.div`
+  height: 16px`
 
 function Tags() {
     const {tags, setTags} = useTags()
     return (
         <Layout>
-            {tags.map(tag => <li key={tag}>{tag}</li>)}
+            <TagList>
+                {tags.map(tag =>
+                    <li key={tag}>
+                        <Link to={'/tags/'+tag}>
+                            <span className='onLine'>{tag}</span>
+                            <Icon name='right'/>
+                        </Link>
+
+                    </li>)}
+            </TagList>
+            <Center>
+                <Space/>
+                <Button> 新增标签</Button>
+            </Center>
         </Layout>
     )
 }
+
 export default Tags
