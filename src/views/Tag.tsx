4127e8a4d32d56f96b9448ef2ebac5f5bb2586cc
chenyuting <357815153@qq.com>
2021-11-21 15:10:57 +0800

    抽离Button

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import useTags from "../useTags";
 import {useParams} from "react-router-dom";
+import Layout from "../components/Layout";
+import Icon from "../components/Icon";
+import {Button} from "../components/Button";
+
 type Params = {
-    id:string
+    id: string
 }
-const Tag:React.FC = ()=>{
-    const {findTag}=useTags()
-    let {id}=useParams<Params>()
+const Tag: React.FC = () => {
+    const {findTag} = useTags()
+    let {id} = useParams<Params>()
     const tag = findTag(parseInt(id))
-    return(
-        <div>{tag.name}</div>
+    return (
+        <Layout>
+            <header>
+                <Icon name='left'/>
+                <span>编辑标签</span>
+            </header>
+            <div>
+                <label>
+                    <span>标签名</span>
+                    <input type='text' placeholder='标签名'
+                    />
+                </label>
+            </div>
+           <Button>删除标签</Button>
+        </Layout>
+
     )
 }
 export {Tag}
