371f26cf225afa7e9ab5c2508040c62a23d20342
chenyuting <357815153@qq.com>
2021-11-23 17:52:03 +0800

    添加 record

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef, useState} from "react";
-import {createId} from "./lib/createId";
-import {useUpdate} from "./hooks/useUpdate";
+import {createId} from "../lib/createId";
+import {useUpdate} from "./useUpdate";
 
 const useTags = () => {//封装一个自定义Hook,且必须用use命名
     const [tags, setTags] = useState<{ id: number, name: string }[]>([])
