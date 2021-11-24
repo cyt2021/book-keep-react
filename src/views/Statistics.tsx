695d0e250bb95bb1195b30d862baf93c1aa1547c
chenyuting <357815153@qq.com>
2021-11-24 16:54:42 +0800

    展示所有记录

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -6,6 +6,19 @@ import {useRecords} from "../hooks/useRecords";
 import useTags from "../hooks/useTags";
 import day from 'dayjs'
 
+ const Item = styled.div`
+ display: flex;
+   justify-content: space-between;
+   background: white;
+   font-size: 18px;
+   line-height: 20px;
+   padding:10px 16px
+ > .note {
+     margin-right: auto;
+     margin-left: 6px;
+     color: #999;
+   }
+ `
 const CategoryWrapper = styled.div`
   background: white
 `
@@ -16,19 +29,23 @@ function Statistics() {
 return (
     <Layout>
         <CategoryWrapper>
-        <CategorySection value={category} onChange={value => {setCategory(value)
-        }}/>
+        <CategorySection value={category} onChange={value => setCategory(value)
+        }/>
         </CategoryWrapper>
         <div>
             {records.map(r=>{
-                return <div>{
-                    r.tagIds.map(tagId=><span>{getName(tagId)}</span>)
-                }
-                <hr/>
-                    {r.amount}
-                <hr/>
-                    {day(r.createdAt).format('YYYY年MM月DD日')}
+                return <Item>
+                    <div className='tags' >
+                    {r.tagIds.map(tagId=><span>{getName(tagId)}</span>)}
                     </div>
+                    {r.note && <div className='note'>
+                        {r.note}
+                    </div>}
+                    <div className='amount'>
+                    ￥{r.amount}
+                    </div>
+                    {/*{day(r.createdAt).format('YYYY年MM月DD日')}*/}
+                    </Item>
             })}
         </div>
             </Layout>
